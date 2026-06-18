fdc5be91984d5c373e7264e10be7007b535e69e0
Ice Lifram <seer.absolutezero@gmail.com>
2026-06-18 19:38:22 +0800

    EDA + Descriptive Analysis of Financial Datasets

--- datasets/financial/financial_analysis.py
+++ datasets/financial/financial_analysis.py
@@ -7,14 +7,15 @@ df = pd.read_csv('financial_impact.csv')
 # print(df)
 
 print("------------------")
-print(df.info())
+print(df.describe())
 
-"""
+
 incident_id = df['incident_id']
 direct_losses = df['direct_loss_usd']
 direct_loss_method = df['direct_loss_method']
 recovery_cost = df['recovery_cost_usd']
 total_loss = df['total_loss_usd']
+inflation_adjusted = df['inflation_adjusted_usd']
 insurance_payout = df['insurance_payout_usd']
 regulatory_fine = df['regulatory_fine_usd']
 total_loss_method = df['total_loss_method']
@@ -22,6 +23,11 @@ ransom_demanded = df['ransom_demanded_usd']
 ransom_paid = df['ransom_paid_usd']
 ransom_source = df['ransom_source']
 
+# Total Loss and adjusted costs by inflation
+print(total_loss.agg(["count", "median", "min", "max", "mean"]))
+print("---------")
+print(inflation_adjusted.agg(["count", "median", "min", "max", "mean"]))
+"""
 print()
 # Direct Losses
 print(direct_losses.describe())
